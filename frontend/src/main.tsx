9bef59856fce6942bb2d353c176fe6391820415d
Obisian-cn <Obisian.RK@Celerinntech.com>
2026-04-22 09:33:55 +0530

    fix/browser-route-update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,15 +4,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './context/AuthContext';
 import App from './App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </QueryClientProvider>
+    <BrowserRouter basename="/lms-fre">
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
